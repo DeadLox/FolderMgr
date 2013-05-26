d2bcc5b2aa4ae2ef1e7a10b6b55d5712a0e0fed6
DeadLox <dead.lox@hotmail.fr>
2013-05-26 22:30:17 +0200

    Add file extension

--- File.class.php
+++ File.class.php
@@ -7,10 +7,27 @@
 class File {
 	private $path;
 	private $name;
+	private $fullName;
+	private $extension;
 
 	public function __construct($path) {
 		$this->path = $path;
-		$this->name = $this->getFileName();
+		$this->fullName = $this->getFileName();
+		$this->getExtAndName();
+	}
+
+	/**
+	 * Récupère l'extension du fichier
+	 */
+	private function getExtAndName() {
+		$pos = strpos($this->fullName, ".");
+		if ($pos === false) {
+			$this->name = $this->fullName;
+			$this->extension = "Inconnue";
+		} else {
+			$this->name = substr($this->fullName, 0, $pos);
+			$this->extension = substr($this->fullName, $pos+1, strlen($this->fullName));
+		}
 	}
 
 	private function getFileName(){
@@ -30,5 +47,8 @@ class File {
 	}
 	public function getName(){
 		return $this->name;
+	}
+	public function getFullName(){
+		return $this->fullName;
 	}
 }
